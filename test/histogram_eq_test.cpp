c0a0dff3b9b62ba78b322f0ea7e870ae0b9cbbd2
Vasco <f.vasco@campus.fct.unl.pt>
2024-05-12 13:20:13 +0100

    First test: image equality

--- test/histogram_eq_test.cpp
+++ test/histogram_eq_test.cpp
@@ -1,14 +1,29 @@
 #include <filesystem>
 #include "gtest/gtest.h"
 #include "histogram_par.h"
+#include "histogram_eq.h"
+#include <wb.h>
 
-using namespace cp_par;
+//using namespace cp_par;
 
 #define DATASET_FOLDER "../dataset/"
 
 TEST(HistogramEq, Input01_4) {
 
     wbImage_t inputImage = wbImport(DATASET_FOLDER "input01.ppm");
-    wbImage_t outputImage = iterative_histogram_equalization(inputImage, 4);
+    wbImage_t outputImagePar = cp_par::iterative_histogram_equalization(inputImage, 4);
+    wbImage_t outputImageSeq = cp::iterative_histogram_equalization(inputImage, 4);
+
+    const int imageWidth = wbImage_getWidth(outputImagePar);
+    const int imageHeight = wbImage_getHeight(outputImagePar);
+
+    float* imageParData = wbImage_getData(outputImagePar);
+    float* imageSeqData = wbImage_getData(outputImageSeq);
+
+    for(int i = 0; i < imageWidth; i++)
+        for(int j = 0; j < imageHeight; j++)
+            EXPECT_EQ(imageParData[i*imageHeight+j],imageSeqData[i*imageHeight+j]);
+
+
     // check if the output image is correct
 }
