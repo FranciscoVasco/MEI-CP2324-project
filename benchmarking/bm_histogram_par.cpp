448ef915766cd502d42083634762976c4edda89e
Tomé Dias <tomedias1711@gmail.com>
2024-05-13 19:52:31 +0200

    Added benchmarking

---
+++ benchmarking/bm_histogram_par.cpp
@@ -0,0 +1,4 @@
+#include "bm_histogram_par.h"
+#include "benchmark/benchmark.h"
+
+BENCHMARK_MAIN();
---
+++ benchmarking/bm_histogram_par.h
@@ -0,0 +1,14 @@
+//
+// Created by tomedias on 5/13/24.
+//
+
+#ifndef PROJECT_BM_HISTOGRAM_PAR_H
+#define PROJECT_BM_HISTOGRAM_PAR_H
+
+
+class bm_histogram_par {
+
+};
+
+
+#endif //PROJECT_BM_HISTOGRAM_PAR_H
