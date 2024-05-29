fbe2d2eff9e80444168e496037a28cfa68898b6a
Vasco <f.vasco@campus.fct.unl.pt>
2024-05-29 19:51:50 +0100

    Benchmark for diff sizes

--- benchmarking/bm_histogram_par.cpp
+++ benchmarking/bm_histogram_par.cpp
@@ -7,38 +7,142 @@
 
 #define DATASET_FOLDER "../dataset/"
 
-wbImage_t inputImage = wbImport(DATASET_FOLDER "input01.ppm");
+wbImage_t inputImage512 = wbImport(DATASET_FOLDER "input01.ppm");
+wbImage_t inputImage1024 = wbImport(DATASET_FOLDER "input01.ppm");
+wbImage_t inputImage2048 = wbImport(DATASET_FOLDER "input01.ppm");
+wbImage_t inputImage4096 = wbImport(DATASET_FOLDER "input01.ppm");
 
-static void BM_Par_Hist(benchmark::State& state){
+static void BM_Par_Hist512(benchmark::State& state){
     for (auto _ : state) {
-        cp_par::iterative_histogram_equalization(inputImage, state.range(0));
+        cp_par::iterative_histogram_equalization(inputImage512, state.range(0));
     }
 }
 
-static void BM_Seq_Hist(benchmark::State& state){
+static void BM_Seq_Hist512(benchmark::State& state){
     for (auto _ : state) {
-        cp::iterative_histogram_equalization(inputImage, state.range(0));
+        cp::iterative_histogram_equalization(inputImage512, state.range(0));
     }
 }
 
-static void BM_CUDA_Hist(benchmark::State& state){
+static void BM_CUDA_Hist512(benchmark::State& state){
     for (auto _ : state) {
-        cuda::iterative_histogram_equalization(inputImage, state.range(0));
+        cuda::iterative_histogram_equalization(inputImage512, state.range(0));
     }
 }
 
-BENCHMARK(BM_Par_Hist)
-->Arg(1)
-->Arg(5)
-->Arg(10);
-BENCHMARK(BM_Seq_Hist)
-->Arg(1)
-->Arg(5)
-->Arg(10);
-BENCHMARK(BM_CUDA_Hist)
-->Arg(1)
-->Arg(5)
-->Arg(10);
+static void BM_Par_Hist1024(benchmark::State& state){
+    for (auto _ : state) {
+        cp_par::iterative_histogram_equalization(inputImage1024, state.range(0));
+    }
+}
 
+static void BM_Seq_Hist1024(benchmark::State& state){
+    for (auto _ : state) {
+        cp::iterative_histogram_equalization(inputImage1024, state.range(0));
+    }
+}
+
+static void BM_CUDA_Hist1024(benchmark::State& state){
+    for (auto _ : state) {
+        cuda::iterative_histogram_equalization(inputImage1024, state.range(0));
+    }
+}
+
+static void BM_Par_Hist2048(benchmark::State& state){
+    for (auto _ : state) {
+        cp_par::iterative_histogram_equalization(inputImage2048, state.range(0));
+    }
+}
+
+static void BM_Seq_Hist2048(benchmark::State& state){
+    for (auto _ : state) {
+        cp::iterative_histogram_equalization(inputImage2048, state.range(0));
+    }
+}
+
+static void BM_CUDA_Hist2048(benchmark::State& state){
+    for (auto _ : state) {
+        cuda::iterative_histogram_equalization(inputImage2048, state.range(0));
+    }
+}
+
+static void BM_Par_Hist4096(benchmark::State& state){
+    for (auto _ : state) {
+        cp_par::iterative_histogram_equalization(inputImage4096, state.range(0));
+    }
+}
+
+static void BM_Seq_Hist4096(benchmark::State& state){
+    for (auto _ : state) {
+        cp::iterative_histogram_equalization(inputImage4096, state.range(0));
+    }
+}
+
+static void BM_CUDA_Hist4096(benchmark::State& state){
+    for (auto _ : state) {
+        cuda::iterative_histogram_equalization(inputImage4096, state.range(0));
+    }
+}
+
+BENCHMARK(BM_Par_Hist512)
+->Arg(1)
+->Arg(5)
+->Arg(10)
+->Arg(20);
+BENCHMARK(BM_Seq_Hist512)
+->Arg(1)
+->Arg(5)
+->Arg(10)
+->Arg(20);
+BENCHMARK(BM_CUDA_Hist512)
+->Arg(1)
+->Arg(5)
+->Arg(10)
+->Arg(20);
+BENCHMARK(BM_Par_Hist1024)
+->Arg(1)
+->Arg(5)
+->Arg(10)
+->Arg(20);
+BENCHMARK(BM_Seq_Hist1024)
+->Arg(1)
+->Arg(5)
+->Arg(10)
+->Arg(20);
+BENCHMARK(BM_CUDA_Hist1024)
+->Arg(1)
+->Arg(5)
+->Arg(10)
+->Arg(20);
+BENCHMARK(BM_Par_Hist2048)
+->Arg(1)
+->Arg(5)
+->Arg(10)
+->Arg(20);
+BENCHMARK(BM_Seq_Hist2048)
+->Arg(1)
+->Arg(5)
+->Arg(10)
+->Arg(20);
+BENCHMARK(BM_CUDA_Hist2048)
+->Arg(1)
+->Arg(5)
+->Arg(10)
+->Arg(20);
+BENCHMARK(BM_Par_Hist4096)
+->Arg(1)
+->Arg(5)
+->Arg(10)
+->Arg(20);
+BENCHMARK(BM_Seq_Hist4096)
+->Arg(1)
+->Arg(5)
+->Arg(10)
+->Arg(20);
+BENCHMARK(BM_CUDA_Hist4096)
+->Arg(1)
+->Arg(5)
+->Arg(10)
+->Arg(20);
 
 BENCHMARK_MAIN();
